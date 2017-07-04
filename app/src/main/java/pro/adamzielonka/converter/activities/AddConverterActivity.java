69d002aa29891ae225363caa42b5de42f049b064
Adam Zielonka <root@adamzielonka.pro>
2017-07-04 18:26:42 +0200

    Load converters from json file

--- app/src/main/java/pro/adamzielonka/converter/activities/AddConverterActivity.java
+++ app/src/main/java/pro/adamzielonka/converter/activities/AddConverterActivity.java
@@ -1,17 +1,39 @@
 package pro.adamzielonka.converter.activities;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import pro.adamzielonka.converter.R;
 import pro.adamzielonka.converter.tools.Theme;
+import pro.adamzielonka.converter.units.concrete.ConcreteMeasure;
+import pro.adamzielonka.converter.units.user.Measure;
 
 public class AddConverterActivity extends AppCompatActivity {
 
+    private static final int READ_REQUEST_CODE = 42;
     private SharedPreferences preferences;
 
     @Override
@@ -36,4 +58,96 @@ public class AddConverterActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void onClickSampleFiles(View v) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://bitbucket.org/adam-zielonka-pro/converters/src"));
+        startActivity(browserIntent);
+    }
+
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+    public void onClickFromJsonFile(View v) {
+        ActivityCompat.requestPermissions(this,
+                PERMISSIONS_STORAGE,
+                REQUEST_EXTERNAL_STORAGE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        if (requestCode == REQUEST_EXTERNAL_STORAGE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("*/*");
+                startActivityForResult(intent, READ_REQUEST_CODE);
+            } else {
+                showInfo(R.string.error_no_permissions, R.color.colorRedPrimary);
+            }
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent resultData) {
+        if (requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            Uri uri;
+            if (resultData != null) {
+                uri = resultData.getData();
+                loadConverter(uri);
+            }
+        }
+    }
+
+    private void loadConverter(Uri uri) {
+        try {
+            InputStream inputStream = getContentResolver().openInputStream(uri);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            Gson gson = new Gson();
+
+            ConcreteMeasure concreteMeasure = gson.fromJson(reader, Measure.class).getConcreteMeasure();
+
+            String fileName = "converter_" + concreteMeasure.getName() + ".json";
+
+            for (int i = 0; isFileExist(fileName); i++) {
+                fileName = "converter_" + concreteMeasure.getName() + "_" + i + ".json";
+            }
+
+            Log.i("COUNT", "loadConverter: " + concreteMeasure.getConcreteUnits().size());
+            if (concreteMeasure.getConcreteUnits().size() == 0) {
+                showInfo(R.string.error_no_units, R.color.colorRedPrimary);
+                return;
+            }
+
+            String json = gson.toJson(concreteMeasure);
+            FileOutputStream out = openFileOutput(fileName, MODE_PRIVATE);
+            out.write(json.getBytes());
+            out.close();
+
+            Intent intent = new Intent(getApplicationContext(), StartActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+            finish();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            showInfo(R.string.error_no_json_file, R.color.colorRedPrimary);
+        } catch (Exception e) {
+            showInfo(R.string.error_no_json_file, R.color.colorRedPrimary);
+        }
+
+    }
+
+    public boolean isFileExist(String fileName) {
+        File file = getBaseContext().getFileStreamPath(fileName);
+        return file.exists();
+    }
+
+    public void showInfo(int msg, int color) {
+        View parentLayout = findViewById(android.R.id.content);
+        Snackbar snackbar = Snackbar.make(parentLayout, msg, Snackbar.LENGTH_LONG).setAction("Action", null);
+        View snackbarView = snackbar.getView();
+        snackbarView.setBackgroundColor(ContextCompat.getColor(this, color));
+        snackbar.show();
+    }
 }
--- app/src/main/java/pro/adamzielonka/converter/activities/StartActivity.java
+++ app/src/main/java/pro/adamzielonka/converter/activities/StartActivity.java
@@ -88,5 +88,4 @@ public class StartActivity extends AppCompatActivity {
         editor.putBoolean("firstRun", true);
         editor.apply();
     }
-
 }
