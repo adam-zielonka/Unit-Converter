6882060d4f25da3f11a5a8552c16c7b83ce100cf
Adam Zielonka <root@adamzielonka.pro>
2017-10-30 10:15:09 +0100

    Moved action from Edit to Detail Measure

--- app/src/main/java/pro/adamzielonka/converter/activities/edit/DetailMeasureActivity.java
+++ app/src/main/java/pro/adamzielonka/converter/activities/edit/DetailMeasureActivity.java
@@ -1,17 +1,32 @@
 package pro.adamzielonka.converter.activities.edit;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.io.OutputStream;
 import java.util.Map;
 
 import pro.adamzielonka.converter.R;
+import pro.adamzielonka.converter.activities.StartActivity;
 import pro.adamzielonka.converter.activities.abstractes.EditActivity;
 import pro.adamzielonka.converter.models.concrete.CUnit;
+import pro.adamzielonka.converter.models.file.Measure;
 import pro.adamzielonka.converter.tools.Language;
 import pro.adamzielonka.items.Item;
 
 import static pro.adamzielonka.converter.names.Code.REQUEST_EDIT_ACTIVITY;
+import static pro.adamzielonka.converter.names.Code.REQUEST_SAVE_TO_DOWNLOAD;
+import static pro.adamzielonka.converter.tools.Message.showError;
+import static pro.adamzielonka.converter.tools.Message.showSuccess;
+import static pro.adamzielonka.converter.tools.Permissions.getReadAndWritePermissionsStorage;
+import static pro.adamzielonka.file.FileTools.toJSON;
+import static pro.adamzielonka.file.Open.openJSON;
+import static pro.adamzielonka.file.Save.isExternalStorageWritable;
 
 public class DetailMeasureActivity extends EditActivity {
 
@@ -19,23 +34,6 @@ public class DetailMeasureActivity extends EditActivity {
     public void addItems() {
         setTitle(R.string.title_activity_detail_measure);
         super.addItems();
-        new Item.Builder(this)
-                .setTitleHeader(R.string.local_settings)
-                .setTitle(R.string.list_own_name_measure)
-                .setSwitcherUpdate(() -> cMeasure.isOwnName)
-                .setSwitcherAction(isOwnName -> cMeasure.isOwnName = isOwnName)
-                .setUpdate(() -> cMeasure.ownName)
-                .setAction((String ownName) -> cMeasure.ownName = ownName)
-                .add(itemsView);
-        new Item.Builder(this)
-                .setTitle(R.string.list_own_lang_measure)
-                .setSwitcherUpdate(() -> cMeasure.isOwnLang)
-                .setSwitcherAction(isOwnLang -> cMeasure.isOwnLang = isOwnLang)
-                .setUpdate(() -> Language.getLanguage(cMeasure.ownLang))
-                .setArray(() -> cMeasure.getGlobalLangs())
-                .setPosition(() -> cMeasure.getOwnLangID())
-                .setAction((Integer position) -> cMeasure.ownLang = cMeasure.getGlobalFromID(position))
-                .add(itemsView);
 
         new Item.Builder(this)
                 .setTitleHeader(R.string.measure_details)
@@ -53,6 +51,24 @@ public class DetailMeasureActivity extends EditActivity {
         new Item.Builder(this)
                 .setTitle(R.string.list_item_languages)
                 .setUpdate(this::getLanguages)
+                .add(itemsView);
+
+        new Item.Builder(this)
+                .setTitleHeader(R.string.local_settings)
+                .setTitle(R.string.list_own_name_measure)
+                .setSwitcherUpdate(() -> cMeasure.isOwnName)
+                .setSwitcherAction(isOwnName -> cMeasure.isOwnName = isOwnName)
+                .setUpdate(() -> cMeasure.ownName)
+                .setAction((String ownName) -> cMeasure.ownName = ownName)
+                .add(itemsView);
+        new Item.Builder(this)
+                .setTitle(R.string.list_own_lang_measure)
+                .setSwitcherUpdate(() -> cMeasure.isOwnLang)
+                .setSwitcherAction(isOwnLang -> cMeasure.isOwnLang = isOwnLang)
+                .setUpdate(() -> Language.getLanguage(cMeasure.ownLang))
+                .setArray(() -> cMeasure.getGlobalLangs())
+                .setPosition(() -> cMeasure.getOwnLangID())
+                .setAction((Integer position) -> cMeasure.ownLang = cMeasure.getGlobalFromID(position))
                 .add(itemsView);
     }
 
@@ -103,9 +119,55 @@ public class DetailMeasureActivity extends EditActivity {
             case R.id.menu_edit_converter:
                 startActivityForResult(setEditIntent(EditMeasureActivity.class), REQUEST_EDIT_ACTIVITY);
                 return true;
+            case R.id.menu_delete_converter:
+                new AlertDialog.Builder(this)
+                        .setTitle(R.string.delete_measure_title)
+                        .setCancelable(true)
+                        .setPositiveButton(R.string.dialog_delete, (d, i) -> {
+                            if (getFileStreamPath(cMeasure.concreteFileName).delete() &&
+                                    getFileStreamPath(cMeasure.userFileName).delete()) {
+                                Intent intent = new Intent(getApplicationContext(), StartActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                startActivity(intent);
+                                finish();
+                            }
+                        }).setNegativeButton(R.string.dialog_cancel, (dialog, which) -> {
+                }).show();
+                return true;
+            case R.id.menu_save_converter:
+                ActivityCompat.requestPermissions(this,
+                        getReadAndWritePermissionsStorage(), REQUEST_SAVE_TO_DOWNLOAD);
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_SAVE_TO_DOWNLOAD) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED && isExternalStorageWritable()) {
+                saveToDownloads();
+            } else {
+                showError(this, R.string.error_no_permissions_to_write_file);
+            }
+        }
+    }
+
+    private void saveToDownloads() {
+        try {
+            String json = toJSON(openJSON(this, cMeasure.userFileName, Measure.class));
+
+            OutputStream out = getContentResolver().openOutputStream(getFileUri(cMeasure.getName(measure.global)));
+            if (out != null) {
+                out.write(json.getBytes());
+                out.close();
+                showSuccess(this, R.string.success_save_to_downloads);
+            } else showError(this, R.string.error_create_file);
+        } catch (Exception e) {
+            e.printStackTrace();
+            showError(this, R.string.error_create_file);
+        }
     }
 
 }
--- app/src/main/java/pro/adamzielonka/converter/activities/edit/EditMeasureActivity.java
+++ app/src/main/java/pro/adamzielonka/converter/activities/edit/EditMeasureActivity.java
@@ -1,36 +1,18 @@
 package pro.adamzielonka.converter.activities.edit;
 
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AlertDialog;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
-import java.io.OutputStream;
-
 import pro.adamzielonka.converter.R;
-import pro.adamzielonka.converter.activities.StartActivity;
 import pro.adamzielonka.converter.activities.abstractes.EditActivity;
 import pro.adamzielonka.converter.adapters.MyArrayAdapter;
-import pro.adamzielonka.converter.models.file.Measure;
 import pro.adamzielonka.converter.models.file.Prefix;
 import pro.adamzielonka.converter.models.file.Unit;
 import pro.adamzielonka.converter.tools.Language;
 import pro.adamzielonka.items.Item;
 import pro.adamzielonka.verification.Tests;
 
-import static pro.adamzielonka.converter.names.Code.REQUEST_SAVE_TO_DOWNLOAD;
 import static pro.adamzielonka.converter.tools.Language.getLanguageWords;
-import static pro.adamzielonka.converter.tools.Message.showError;
-import static pro.adamzielonka.converter.tools.Message.showSuccess;
-import static pro.adamzielonka.converter.tools.Permissions.getReadAndWritePermissionsStorage;
-import static pro.adamzielonka.file.FileTools.toJSON;
-import static pro.adamzielonka.file.Open.openJSON;
-import static pro.adamzielonka.file.Save.isExternalStorageWritable;
 
 public class EditMeasureActivity extends EditActivity {
 
@@ -54,20 +36,6 @@ public class EditMeasureActivity extends EditActivity {
                 textSecondary.setText(prefixes.toString());
             }
         };
-
-        new Item.Builder(this)
-                .setTitleHeader(R.string.list_title_language)
-                .setTitle(R.string.list_item_language_available)
-                .setUpdate(() -> cMeasure.languages.toString())
-                .setAction(() -> startEditActivity(EditLanguagesActivity.class))
-                .add(itemsView);
-        new Item.Builder(this)
-                .setTitle(R.string.list_item_language_global)
-                .setUpdate(() -> Language.getLanguage(cMeasure.global))
-                .setArray(() -> cMeasure.getGlobalLangs())
-                .setPosition(() -> cMeasure.getGlobalID())
-                .setAction((Integer id) -> measure.global = cMeasure.getGlobalFromID(id))
-                .add(itemsView);
 
         new Item.Builder(this)
                 .setTitleHeader(R.string.list_title_Measure)
@@ -101,6 +69,20 @@ public class EditMeasureActivity extends EditActivity {
                 .add(itemsView);
 
         new Item.Builder(this)
+                .setTitleHeader(R.string.list_title_language)
+                .setTitle(R.string.list_item_language_available)
+                .setUpdate(() -> cMeasure.languages.toString())
+                .setAction(() -> startEditActivity(EditLanguagesActivity.class))
+                .add(itemsView);
+        new Item.Builder(this)
+                .setTitle(R.string.list_item_language_global)
+                .setUpdate(() -> Language.getLanguage(cMeasure.global))
+                .setArray(() -> cMeasure.getGlobalLangs())
+                .setPosition(() -> cMeasure.getGlobalID())
+                .setAction((Integer id) -> measure.global = cMeasure.getGlobalFromID(id))
+                .add(itemsView);
+
+        new Item.Builder(this)
                 .setTitleHeader(R.string.list_title_units)
                 .setAdapter(adapter)
                 .setUpdateAdapter(() -> measure.units)
@@ -123,66 +105,6 @@ public class EditMeasureActivity extends EditActivity {
         itemsView.onSave();
         unit = unitTemp;
         startEditActivity(EditUnitActivity.class);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_edit_measure, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        switch (id) {
-            case R.id.menu_delete_converter:
-                new AlertDialog.Builder(this)
-                        .setTitle(R.string.delete_measure_title)
-                        .setCancelable(true)
-                        .setPositiveButton(R.string.dialog_delete, (d, i) -> {
-                            if (getFileStreamPath(cMeasure.concreteFileName).delete() &&
-                                    getFileStreamPath(cMeasure.userFileName).delete()) {
-                                Intent intent = new Intent(getApplicationContext(), StartActivity.class);
-                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                                startActivity(intent);
-                                finish();
-                            }
-                        }).setNegativeButton(R.string.dialog_cancel, (dialog, which) -> {
-                }).show();
-                return true;
-            case R.id.menu_save_converter:
-                ActivityCompat.requestPermissions(this,
-                        getReadAndWritePermissionsStorage(), REQUEST_SAVE_TO_DOWNLOAD);
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
-        if (requestCode == REQUEST_SAVE_TO_DOWNLOAD) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED && isExternalStorageWritable()) {
-                saveToDownloads();
-            } else {
-                showError(this, R.string.error_no_permissions_to_write_file);
-            }
-        }
-    }
-
-    private void saveToDownloads() {
-        try {
-            String json = toJSON(openJSON(this, cMeasure.userFileName, Measure.class));
-
-            OutputStream out = getContentResolver().openOutputStream(getFileUri(cMeasure.getName(measure.global)));
-            if (out != null) {
-                out.write(json.getBytes());
-                out.close();
-                showSuccess(this, R.string.success_save_to_downloads);
-            } else showError(this, R.string.error_create_file);
-        } catch (Exception e) {
-            e.printStackTrace();
-            showError(this, R.string.error_create_file);
-        }
     }
 
 }
